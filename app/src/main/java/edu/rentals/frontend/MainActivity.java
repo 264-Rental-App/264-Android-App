139c3fcfe87be3207af803786b63f9d3cb7af75c
angeljiang0927@gmail.com <angeljiang0927@gmail.com>
2021-02-13 14:49:11 -0800

    created landing view according to prototype

--- app/src/main/java/edu/rentals/frontend/MainActivity.java
+++ app/src/main/java/edu/rentals/frontend/MainActivity.java
@@ -2,7 +2,11 @@ package edu.rentals.frontend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv = findViewById(R.id.logo);
+//        View view = findViewById(R.id.relativeLayout);
+//        view.setBackgroundColor(Color.parseColor("#ffd59e"));
     }
 }
