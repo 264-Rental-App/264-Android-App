c7ba0ca866e610af487ac2eff27052279d5dca76
angeljiang0927@gmail.com <angeljiang0927@gmail.com>
2021-02-13 19:16:52 -0800

    completed landing page view created searchShopListActivity

---
+++ app/src/main/java/edu/rentals/frontend/SearchShopActivity.java
@@ -0,0 +1,14 @@
+package edu.rentals.frontend;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SearchShopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+}
