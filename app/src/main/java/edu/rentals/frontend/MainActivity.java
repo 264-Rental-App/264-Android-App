e8a4165e14eb139a087df2f44a6088e0f6cdf57d
TssT96 <42284547+TssT96@users.noreply.github.com>
2021-03-08 03:35:19 -0800

    Boardcast function added
    Boardcast function network status check, remaining time remainder

--- app/src/main/java/edu/rentals/frontend/MainActivity.java
+++ app/src/main/java/edu/rentals/frontend/MainActivity.java
@@ -1,6 +1,9 @@
 package edu.rentals.frontend;
 
+import android.content.BroadcastReceiver;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,11 +21,20 @@ public class MainActivity extends AppCompatActivity {
     Button srcButton;
     EditText inputField;
     Button loginButton;
+    BroadcastReceiver broadcastReceiver;
+    BroadcastReceiver broadcastReceiver_t;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Timer boardcast reciver
+        broadcastReceiver_t = new RentalTimerReceiver();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_SCREEN_ON);
+        registerReceiver(broadcastReceiver_t, intentFilter);
+
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -75,10 +87,13 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-
     @Override
     public void onStart() {
         super.onStart();
+
+        //network connection status boardcast reciver
+        broadcastReceiver = new InternetstateReceiver();
+        registerReceiver(broadcastReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
 
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
@@ -89,6 +104,26 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        try{
+            unregisterReceiver(broadcastReceiver);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        try{
+            unregisterReceiver(broadcastReceiver_t);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 
     private void goHome() {
         startActivity(new Intent(this, CustomerHomeActivity.class));
